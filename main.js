cfef8217e3bd34fc6064cdeed43c5571c0e0fd7a
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-03-22 20:17:56 -0500

    other js
    Modificacion de js

--- main.js
+++ main.js
@@ -1,84 +1,72 @@
-// Seleccionamos los elementos del DOM que necesitamos
+//Seleccionamos los elementos del DOM que necesitamos
 const carousel = document.querySelector('.carousel-items');
 const prevBtn = document.querySelector('.carousel-prev');
 const nextBtn = document.querySelector('.carousel-next');
 
-// Establecemos el número de elementos por página y el índice actual
+//Establecemos el número de elementos por página y el índice actual
 const itemsPerPage = 4;
 let currentPage = 1;
 
-// Calculamos el número total de páginas
+//Calculamos el número total de páginas
 const totalPages = Math.ceil(carousel.children.length / itemsPerPage);
 
-// Función que actualiza el carrusel al cambiar de página
+//Función que actualiza el carrusel al cambiar de página
 function updateCarousel() {
-  // Ocultamos todos los elementos
-  for (let i = 0; i < carousel.children.length; i++) {
-    carousel.children[i].style.display = 'none';
-  }
-
-  // Mostramos los elementos de la página actual
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  for (let i = startIndex; i < endIndex && i < carousel.children.length; i++) {
-    carousel.children[i].style.display = 'flex';
-  }
+//Ocultamos todos los elementos
+for (let i = 0; i < carousel.children.length; i++) {
+carousel.children[i].style.display = 'none';
+}
+//Mostramos los elementos de la página actual
+const startIndex = (currentPage - 1) * itemsPerPage;
+const endIndex = startIndex + itemsPerPage;
+for (let i = startIndex; i < endIndex && i < carousel.children.length; i++) {
+carousel.children[i].style.display = 'flex';
+}
 }
 
-// Inicializamos el carrusel mostrando la primera página
+//Inicializamos el carrusel mostrando la primera página
 updateCarousel();
 
-// Evento del botón "Siguiente"
-nextBtn.addEventListener('click', function() {
-  if (currentPage < totalPages) {
-    currentPage++;
-    updateCarousel();
-  }
-});
-
-// Evento del botón "Anterior"
-prevBtn.addEventListener('click', function() {
-  if (currentPage > 1) {
-    currentPage--;
-    updateCarousel();
-  }
-});
-
-// Get the modal element
-var modal = document.getElementById("modal");
-
-// Get the button that opens the modal
-var showModalBtn = document.getElementById("show-modal-btn");
-
-// When the user clicks the button, open the modal
-showModalBtn.onclick = function() {
-  modal.style.display = "block";
+//Evento del botón "Siguiente"
+nextBtn.addEventListener('click', () => {
+if (currentPage < totalPages) {
+currentPage++;
+updateCarousel();
 }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-$(document).ready(function() {
-  // Obtener referencias a los elementos relevantes del DOM
-  var $signupLink = $('#signup-link');
-  var $signupModal = $('#signup-modal');
-  var $signupModalClose = $signupModal.find('.logIn_btn-changes1');
-
-  // Manejar el evento click en el enlace de registro
-  $signupLink.on('click', function(event) {
-    event.preventDefault();
-    $signupModal.fadeIn();
-  });
-
-  // Manejar el evento click en el botón de cerrar del formulario
-  $signupModalClose.on('click', function(event) {
-    event.preventDefault();
-    $signupModal.fadeOut();
-  });
 });
 
+//Evento del botón "Anterior"
+prevBtn.addEventListener('click', () => {
+if (currentPage > 1) {
+currentPage--;
+updateCarousel();
+}
+});
 
+//Obtener referencia al modal y el botón que lo abre
+const modal = document.getElementById('modal');
+const showModalBtn = document.getElementById('show-modal-btn');
+
+//Abrir el modal cuando se hace clic en el botón
+showModalBtn.addEventListener('click', () => {
+modal.style.display = 'block';
+});
+
+//Cerrar el modal cuando se hace clic fuera de él
+window.addEventListener('click', (event) => {
+if (event.target == modal) {
+modal.style.display = 'none';
+}
+});
+
+//Manejar el evento click en el enlace de registro
+$('#signup-link').on('click', (event) => {
+event.preventDefault();
+$('#signup-modal').fadeIn();
+});
+
+//Manejar el evento click en el botón de cerrar del formulario
+$('#signup-modal .logIn_btn-changes1').on('click', (event) => {
+event.preventDefault();
+$('#signup-modal').fadeOut();
+});
